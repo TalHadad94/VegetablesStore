7f57e5bf96d79d276b1be261539d1e0b4b2bc9a5
Tal <talsmd95@gmail.com>
2025-01-25 06:20:29 +0200

    Add sticky navbar by JS

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Creating a Sticky Navbar
+let navbar = document.getElementById("main-nav");
+let navPos = navbar.getBoundingClientRect().top;
+
+window.addEventListener("scroll", () => {
+let scrollPos = window.scrollY;
+if (scrollPos > navPos) {
+navbar.classList.add("sticky");
+} else {
+navbar.classList.remove("sticky");
+}
+});
+
 // Show the active main section with error handling and optimizations
 document.querySelectorAll('nav ul li a').forEach(link => {
     link.addEventListener('click', function(e) {
